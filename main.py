aee956a1deb51bcc80dc7400ac38244784a5b609
Rafael Lima Barreto <rafaellbecom@gmail.com>
2023-06-20 19:32:03 -0300

    Desenvolvimento FastAPI

---
+++ main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+from baseDeDados import avaliacoesUsuario
+from baseDeDadosInvertida import avaliacoesFilmes
+from recomendacao import *
+
+
+app = FastAPI()
+recomendation = None
+
+# Configuração do CORS
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://127.0.0.1:5500"
+    # Adicione aqui as origens permitidas (URLs) para acesso à sua API
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
+
+class User(BaseModel):
+    user_name: str
+
+@app.post('/')
+def sendUser(user:User):
+    base = carregaMovieLens()  
+    global recomendation
+    recomendation = getRecomendacoesUsuario(base, user.user_name)
+    return recomendation
+
+
+@app.get('/')
+def getRecomendations():
+    global recomendation
+
+    return {
+        'recomendations': recomendation
+    }
